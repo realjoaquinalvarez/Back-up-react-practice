aac6bc855edf6c20a7fc880e99cc6097cac868c3
Joaquin <clcflecha@gmail.com>
2024-06-26 06:13:27 -0400

    copia de seguridad

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-
-      <nav>
-        <Link to={"/auth/register"}>No tienes una cuenta?</Link>
+      <h1 className="text-4xl text-white font-bold">Iniciar Sesion</h1>
+    
+      <nav className="mt-10">
+        <Link 
+          className="text-center text-white text-lg block" 
+          to={"/auth/register"}
+        >No tienes una cuenta?</Link>
       </nav>
     </>
   );
